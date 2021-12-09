b0c412e2d88b578be7c980e78ddba61ef1c96c33
James Slepicka <james@slepicka.net>
2021-12-09 02:57:22 -0600

    day 9 cleanup

--- 9.py
+++ 9.py
@@ -4,8 +4,10 @@ neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 def add_points(p1, p2):
     return (p1[0] + p2[0], p1[1] + p2[1])
 
-def get_low_points(input, width, height):
+def get_low_points(input):
     low_points = []
+    width = max(input, key=lambda p: p[0])[0] + 1
+    height = max(input, key=lambda p: p[1])[1] + 1
     for y in range(height):
         for x in range(width):
             p = (x, y)
@@ -19,9 +21,9 @@ def get_low_points(input, width, height):
                 low_points.append(p)
     return low_points
 
-def part1(input, width, height):
+def part1(input):
     result = 0
-    for p in get_low_points(input, width, height):
+    for p in get_low_points(input):
         result += input[p] + 1
     return result
 
@@ -39,9 +41,9 @@ def get_basin_depth(input, low_point):
                 queue.insert(0, p2)
     return len(visited)
 
-def part2(input, width, height):
+def part2(input):
     depths = []
-    for p in get_low_points(input, width, height):
+    for p in get_low_points(input):
         depths.append(get_basin_depth(input, p))
     depths.sort(reverse=True)
     result = 1
@@ -51,7 +53,6 @@ def part2(input, width, height):
 
 def main():
     input = defaultdict(lambda: 10)
-    height = 0
     with open("9.txt") as file:
         y = 0
         for line in  [x.strip() for x in file.readlines()]:
@@ -59,11 +60,9 @@ def main():
             for i in list(line):
                 input[(x, y)] = int(i)
                 x += 1
-            width = x
             y += 1
-        height = y
 
-    print("Part 1: " + str(part1(input, width, height)))
-    print("Part 2: " + str(part2(input, width, height)))
+    print("Part 1: " + str(part1(input)))
+    print("Part 2: " + str(part2(input)))
 if __name__ == "__main__":
     main()
